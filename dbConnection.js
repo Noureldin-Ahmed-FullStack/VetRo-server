448e53d9390322c64a65153fe739b5167e3c49f0
Noureldin-Ahmed-FullStack <noureldin2662002@gmail.com>
2024-04-09 17:48:22 +0200

    changed Database

--- dbConnection.js
+++ dbConnection.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 export function dbConnection() {
-    return mongoose.connect('mongodb+srv://noureldin:2662002nour@cluster0.ghoe1dy.mongodb.net/Ecommarce').then(()=>{
-        console.log("Ecommarce DB connected");
+    return mongoose.connect('mongodb+srv://noureldin:2662002nour@cluster0.ghoe1dy.mongodb.net/VetRo').then(()=>{
+        console.log("VetRo DB connected");
     }).catch((err)=>{
         console.log("DB error "+err);
     })
